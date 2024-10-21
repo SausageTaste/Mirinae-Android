87f32d76b1f2da1367daf4509e998e85822b5614
Sungmin Woo <woos8899@gmail.com>
2024-10-21 21:00:57 +0900

    Move vcpkg install dir

--- app/src/main/cpp/vcpkg_install.bat
+++
@@ -1,2 +0,0 @@
-vcpkg install --triplet arm64-android    --x-install-root=vcpkg_installed_arm64-android
-vcpkg install --triplet x64-android      --x-install-root=vcpkg_installed_x64-android
---
+++ app/vcpkg_install.bat
@@ -0,0 +1,2 @@
+vcpkg install --triplet arm64-android    --x-install-root=vcpkg_installed/arm64-android
+vcpkg install --triplet x64-android      --x-install-root=vcpkg_installed/x64-android
