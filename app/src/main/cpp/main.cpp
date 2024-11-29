6ad783d82a3fe88f00d16f8e49a15f8b52db4653
Sungmin Woo <woos8899@gmail.com>
2024-11-30 02:47:26 +0900

    Journey of fixing crash

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -18,101 +18,26 @@
 
 namespace {
 
-    class LogcatSink : public spdlog::sinks::base_sink<std::mutex> {
-
-    public:
-        LogcatSink() = default;
-
-    protected:
-        void sink_it_(const spdlog::details::log_msg& msg) override {
-            spdlog::memory_buf_t formatted;
-            spdlog::sinks::base_sink<std::mutex>::formatter_->format(msg, formatted);
-            const auto text = fmt::to_string(formatted);
-            const auto log_level = this->log_level_cast(msg.level);
-            __android_log_print(log_level, "Mirinae", "%s\n", text.c_str());
-        }
-
-        void flush_() override {
-
-        }
-
-    private:
-        static android_LogPriority log_level_cast(spdlog::level::level_enum e) {
-            switch (e) {
-                case spdlog::level::level_enum::trace:
-                    return ANDROID_LOG_VERBOSE;
-                case spdlog::level::level_enum::debug:
-                    return ANDROID_LOG_DEBUG;
-                case spdlog::level::level_enum::info:
-                    return ANDROID_LOG_INFO;
-                case spdlog::level::level_enum::warn:
-                    return ANDROID_LOG_WARN;
-                case spdlog::level::level_enum::err:
-                    return ANDROID_LOG_ERROR;
-                case spdlog::level::level_enum::critical:
-                    return ANDROID_LOG_FATAL;
-                case spdlog::level::level_enum::off:
-                    return ANDROID_LOG_SILENT;
-                default:
-                    return ANDROID_LOG_UNKNOWN;
-            }
-        }
-
-    };
+    std::shared_ptr<spdlog::logger> g_android_logger;
 
 
     class CombinedEngine {
 
     public:
-        CombinedEngine(android_app* const state) {
+        explicit CombinedEngine(android_app* const state) {
             // Logger
-            {
-                auto android_logger = spdlog::android_logger_mt("android", "Mirinae");
-                spdlog::set_default_logger(android_logger);
+            if (!g_android_logger) {
+                g_android_logger = spdlog::android_logger_mt("android", "Mirinae");
+                spdlog::set_default_logger(g_android_logger);
                 spdlog::set_level(spdlog::level::debug);
-            }
-
-            {
-                auto dir = AAssetManager_openDir(state->activity->assetManager, "textures");
-                if (dir) {
-                    AAssetDir_rewind(dir);
-                    while (auto entry = AAssetDir_getNextFileName(dir)) {
-                        spdlog::info("Entry: {}", entry);
-                    }
-                }
-
-                char lang[3] = {};
-                AConfiguration_getLanguage(state->config, lang);
-                lang[2] = '\0';
-                spdlog::info("Config language: {}", lang);
-
-                AConfiguration_getCountry(state->config, lang);
-                lang[2] = '\0';
-                spdlog::info("Config country: {}", lang);
-
-                ACONFIGURATION_SCREEN_WIDTH_DP_ANY;
-                spdlog::info("Config DP: {} x {}", AConfiguration_getScreenWidthDp(state->config), AConfiguration_getScreenHeightDp(state->config));
-
-                const auto int_data_path = std::filesystem::u8path(state->activity->internalDataPath);
-                const auto int_file_path = int_data_path / "shit.txt";
-                std::ofstream internal_file{int_file_path};
-                internal_file << "Shit";
-                internal_file.close();
-                spdlog::info("File created: {}", int_file_path.u8string());
-
-                const auto ext_data_path = std::filesystem::u8path(state->activity->externalDataPath);
-                const auto ext_file_path = ext_data_path / "shit.txt";
-                std::ofstream ext_file{ext_file_path};
-                ext_file << "Dayum";
-                ext_file.close();
-                spdlog::info("File created: {}", ext_file_path.u8string());
-
-                spdlog::info("OBB path: {}", state->activity->obbPath);
             }
 
             create_info_.filesys_ = std::make_shared<dal::Filesystem>();
             create_info_.filesys_->add_subsys(mirinapp::create_filesubsys_android_asset(
                 state->activity->assetManager, *create_info_.filesys_
+            ));
+            create_info_.filesys_->add_subsys(dal::create_filesubsys_std(
+                "", ::std::filesystem::u8path(state->activity->externalDataPath), *create_info_.filesys_
             ));
 
             create_info_.instance_extensions_ = std::vector<std::string>{
@@ -155,6 +80,10 @@ namespace {
             return true;
         }
 
+        void on_resize(uint32_t w, uint32_t h) {
+            engine_->notify_window_resize(w, h);
+        }
+
     private:
         mirinae::EngineCreateInfo create_info_;
         std::unique_ptr<mirinae::IEngine> engine_;
@@ -164,28 +93,44 @@ namespace {
 }
 
 
+void on_content_rect_changed(android_app* const state) {
+    if (nullptr == state->userData)
+        return;
+
+    auto& engine = *reinterpret_cast<::CombinedEngine*>(state->userData);
+    const auto width  = state->contentRect.right  - state->contentRect.left;
+    const auto height = state->contentRect.bottom - state->contentRect.top;
+
+    engine.on_resize(width, height);
+}
+
+
 extern "C" {
 
 void handle_cmd(android_app *pApp, int32_t cmd) {
     switch (cmd) {
         case APP_CMD_INIT_WINDOW:
-            // A new window is created, associate a renderer with it. You may replace this with a
-            // "game" class if that suits your needs. Remember to change all instances of userData
-            // if you change the class here as a reinterpret_cast is dangerous this in the
-            // android_main function and the APP_CMD_TERM_WINDOW handler case.
-            pApp->userData = new ::CombinedEngine(pApp);
-            break;
-        case APP_CMD_TERM_WINDOW:
-            // The window is being destroyed. Use this to clean up your userData to avoid leaking
-            // resources.
-            //
-            // We have to check if userData is assigned just in case this comes in really quickly
+            spdlog::info("APP_CMD_INIT_WINDOW");
             if (pApp->userData) {
                 auto engine = reinterpret_cast<::CombinedEngine*>(pApp->userData);
                 delete engine;
             }
+            pApp->userData = new ::CombinedEngine(pApp);
+            break;
+        case APP_CMD_CONTENT_RECT_CHANGED:
+            spdlog::info("APP_CMD_CONTENT_RECT_CHANGED");
+            on_content_rect_changed(pApp);
+            break;
+        case APP_CMD_TERM_WINDOW:
+            spdlog::info("APP_CMD_TERM_WINDOW");
+            if (pApp->userData) {
+                auto engine = reinterpret_cast<::CombinedEngine*>(pApp->userData);
+                delete engine;
+            }
+            pApp->userData = nullptr;
             break;
         default:
+            spdlog::info("APP_CMD ({}): unhandled", static_cast<int>(cmd));
             break;
     }
 }
